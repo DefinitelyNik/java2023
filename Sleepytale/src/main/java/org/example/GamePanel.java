14a4893b856f9178f64e213aa99496e0c769d139
Nik <45146898+DefinitelyNik@users.noreply.github.com>
2023-04-02 06:58:08 +0500

    Created game loop(u can move the player character(rectangle for now))

--- Sleepytale/src/main/java/org/example/GamePanel.java
+++ Sleepytale/src/main/java/org/example/GamePanel.java
@@ -14,12 +14,23 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 1024 пикселей
     final int screenHeight = tileSize * maxScreenRow; // 768 пикселей
 
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -30,5 +41,62 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/FPS; // 0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+
+        while(gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            /**
+             * Checking the actual FPS
+             */
+            if(timer >= 1000000000) {
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+    public void update(){
+
+        if(keyH.upPressed) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
     }
 }
