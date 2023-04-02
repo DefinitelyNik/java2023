38f4103aed875c554627039c6d0c3364171daf6b
Nik <45146898+DefinitelyNik@users.noreply.github.com>
2023-04-02 22:26:30 +0500

    Added player model and animations -replaced white rectangle with actual player model(png image) -added animations(they're not working cause we have only 1 player model image for now) -also created entity class

--- Sleepytale/src/main/java/org/example/GamePanel.java
+++ Sleepytale/src/main/java/org/example/GamePanel.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable{
     //настройки
     final int originalTileSize = 64; // 64х64 - оригинальный размер плитки
     final int scale = 1;
-    final int tileSize = originalTileSize * scale; // 64х64 - итоговый размер плитки
+    public final int tileSize = originalTileSize * scale; // 64х64 - итоговый размер плитки
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 1024 пикселей
@@ -19,6 +21,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     // Set player's default position
     int playerX = 100;
@@ -74,19 +77,7 @@ public class GamePanel extends JPanel implements Runnable{
         }
     }
     public void update(){
-
-        if(keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -95,8 +86,8 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
+
         g2.dispose();
     }
 }
