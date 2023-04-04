b71ca0feeabde525a5aeac3fc5f063fa69aefc40
Nik <45146898+DefinitelyNik@users.noreply.github.com>
2023-04-05 02:36:53 +0500

    Added entity collision Made collision rectangle(which is smaller than the entity's model)

--- Sleepytale/src/main/java/entity/Player.java
+++ Sleepytale/src/main/java/entity/Player.java
@@ -23,6 +23,8 @@ public class Player extends Entity{
         screenX = gp.screenWidth/2 - (gp.tileSize/2);
         screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
+        solidArea = new Rectangle(26, 32, 12, 24);
+
         setDefaultValues();
         getPlayerImage();
     }
@@ -55,19 +57,29 @@ public class Player extends Entity{
 
             if(keyH.upPressed) {
                 direction = "up";
-                worldY -= speed;
             }
             else if(keyH.downPressed) {
                 direction = "down";
-                worldY += speed;
             }
             else if(keyH.leftPressed) {
                 direction = "left";
-                worldX -= speed;
             }
             else if(keyH.rightPressed) {
                 direction = "right";
-                worldX += speed;
+            }
+
+            // Проверка коллизии плитки
+            collisionOn = false;
+            gp.cChecker.CheckTile(this);
+
+            // Если коллизии нет, игрок может двигаться
+            if(!collisionOn) {
+                switch (direction) {
+                    case "up" -> worldY -= speed;
+                    case "down" -> worldY += speed;
+                    case "left" -> worldX -= speed;
+                    case "right" -> worldX += speed;
+                }
             }
 
             spriteCounter++;
@@ -85,39 +97,39 @@ public class Player extends Entity{
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
 
-        switch(direction) {
-            case "up":
-                if(spriteNum == 1){
+        switch (direction) {
+            case "up" -> {
+                if (spriteNum == 1) {
                     image = up1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = up2;
                 }
-                break;
-            case "down":
-                if(spriteNum == 1){
+            }
+            case "down" -> {
+                if (spriteNum == 1) {
                     image = down1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = down2;
                 }
-                break;
-            case "left":
-                if(spriteNum == 1){
+            }
+            case "left" -> {
+                if (spriteNum == 1) {
                     image = left1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = left2;
                 }
-                break;
-            case "right":
-                if(spriteNum == 1){
+            }
+            case "right" -> {
+                if (spriteNum == 1) {
                     image = right1;
                 }
-                if(spriteNum == 2){
+                if (spriteNum == 2) {
                     image = right2;
                 }
-                break;
+            }
         }
         g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
---
+++ Sleepytale/src/main/java/org/game/CollisionChecker.java
@@ -0,0 +1,62 @@
+package org.game;
+
+import entity.Entity;
+
+public class CollisionChecker {
+
+    GamePanel gp;
+
+    public CollisionChecker(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void CheckTile(Entity entity) {
+
+        int entityLeftWorldX = entity.worldX + entity.solidArea.x; // координата левого края области коллизии
+        int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width; // координата правого края области коллизии
+        int entityTopWorldY = entity.worldY + entity.solidArea.y; // координата верхнего края области коллизии
+        int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height; // координата нижнего края области коллизии
+
+        int entityLeftCol = entityLeftWorldX/gp.tileSize;
+        int entityRightCol = entityRightWorldX/gp.tileSize;
+        int entityTopRow = entityTopWorldY/gp.tileSize;
+        int entityBottomRow = entityBottomWorldY/gp.tileSize;
+
+        int tileNum1, tileNum2; // две координаты стороны области коллизии
+
+        switch (entity.direction) {
+            case "up" -> {
+                entityTopRow = (entityTopWorldY - entity.speed) / gp.tileSize;
+                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
+                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityTopRow];
+                if (gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+            }
+            case "down" -> {
+                entityBottomRow = (entityBottomWorldY + entity.speed) / gp.tileSize;
+                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
+                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
+                if (gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+            }
+            case "left" -> {
+                entityLeftCol = (entityLeftWorldX - entity.speed) / gp.tileSize;
+                tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
+                tileNum2 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
+                if (gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+            }
+            case "right" -> {
+                entityRightCol = (entityRightWorldX + entity.speed) / gp.tileSize;
+                tileNum1 = gp.tileM.mapTileNum[entityRightCol][entityTopRow];
+                tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
+                if (gp.tileM.tile[tileNum1].collision || gp.tileM.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+            }
+        }
+    }
+}
