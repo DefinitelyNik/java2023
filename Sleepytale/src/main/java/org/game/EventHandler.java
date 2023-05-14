8659323d8c45d5d3e6254e7a84d613441f24d80e
Nik <45146898+DefinitelyNik@users.noreply.github.com>
2023-05-14 22:08:12 +0500

    Updated event system -made it more flexible

--- Sleepytale/src/main/java/org/game/EventHandler.java
+++ Sleepytale/src/main/java/org/game/EventHandler.java
@@ -4,24 +4,48 @@ import java.awt.*;
 
 public class EventHandler {
     GamePanel gp;
-    Rectangle eventRect; // квадрат который будет указывать точку триггера ивента
-    int eventRectDefaultX, eventRectDefaultY; // деволтные координаты x и y lkz eventRect
+    EventRect eventRect[][];
+    int previousEventX, previousEventY;
+    boolean canTouchEvent = true;
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
 
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = gp.tileSize;
-        eventRect.height = gp.tileSize;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+        int col = 0;
+        int row = 0;
+        while(col < gp.maxWorldCol && row < gp.maxWorldRow) {
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = gp.tileSize;
+            eventRect[col][row].height = gp.tileSize;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if(col == gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
+        }
     }
 
     public void checkEvent() {
-        if(hit(27,16,"right")) {damagePit(gp.dialogueState);}
-        if (hit(23, 12, "up")) {healingPool(gp.dialogueState);}
+
+        // Проверяем, находится ли игрок на расстроянии 1 плитки от последнего ивента
+        int xDistance = Math.abs(gp.player.worldX - previousEventX);
+        int yDistance = Math.abs(gp.player.worldY - previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+        if(distance > gp.tileSize) {
+            canTouchEvent = true;
+        }
+
+        // Ивенты
+        if(canTouchEvent) {
+            if(hit(27,16,"right")) {damagePit(27, 16, gp.dialogueState);}
+            if(hit(23, 12, "up")) {healingPool(23, 12, gp.dialogueState);}
+        }
     }
 
     /**
@@ -29,24 +53,27 @@ public class EventHandler {
      * Также метод проверяет, куда смотрит игрок(таким образом, можно реализовать триггер ивента,
      * только если игрок смотрит в правильную сторону и находится на желаемой плитке)
      */
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if(gp.player.solidArea.intersects(eventRect)) {
+        if(gp.player.solidArea.intersects(eventRect[col][row]) && !eventRect[col][row].eventDone) {
             if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
+
+                previousEventX = gp.player.worldX;
+                previousEventY = gp.player.worldY;
             }
         }
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
@@ -55,17 +82,19 @@ public class EventHandler {
      * Метод, который по своей сути является ивентом, наносящим урон игроку
      * Игрок должен встать на определенную плитку, которая нанесет ему урон
      */
-    public void damagePit(int gameState) {
+    public void damagePit(int col, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You fall into a pit..";
         gp.player.life -= 1;
+        //eventRect[col][row].eventDone = true;
+        canTouchEvent = false;
     }
 
     /**
      * Метод, который по своей сути является ивентом, пополняющим очки здоровья игрока
      * Игрок должен подойти к определенной плитке, нажать на кнопку, после чего его здоровье станет максимальным
      */
-    public void healingPool(int gameState) {
+    public void healingPool(int col, int row, int gameState) {
         if(gp.keyH.ePressed) {
             gp.gameState = gameState;
             gp.ui.currentDialogue = "You drink the water. You've gained some hp!";
---
+++ Sleepytale/src/main/java/org/game/EventRect.java
@@ -0,0 +1,9 @@
+package org.game;
+
+import java.awt.*;
+
+public class EventRect extends Rectangle {
+    int eventRectDefaultX, eventRectDefaultY; // дефолтные координаты x и y для eventRect
+    boolean eventDone = false;
+
+}
